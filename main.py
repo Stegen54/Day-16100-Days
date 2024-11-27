0f9f94ebc31d162dd6d3ad298f6102403e015d00
Onyeali Emmanuel <onyealistegen54@gmail.com>
2024-11-27 21:34:28 +0000

    feat: Implement interactive song lyric guessing game
    Add a Python program for a "Name the Song Lyric" game. The game
    prompts the user to guess a missing word in a song lyric, provides
    feedback for incorrect guesses, and tracks the number of attempts
    before a correct answer.
    
    This feature enhances interactivity and engagement with fun
    guess-the-word gameplay.
    
    Resolves: #1

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+print("Welcome to Name the Song Lyric")
+print()
+print("Figure out the missing word as quickly as you can!")
+print()
+
+counter = 1  # Keeps track of attempts
+
 while True:
-  print("This program is running")
-  goAgain = input("Go again?: ")
-  if goAgain == "no":
-    break
-print("Aww, I was having a good time 😭")
+    # Prompt the user to guess the missing word
+    lyrics = input("We don't talk about ______. ").strip()
+    # Check if the user guessed correctly
+    if lyrics.lower() == "bruno":
+        print("You got it!")
+        break
+    else:
+        print("Nope! Try again!")
+        counter += 1
+
+print("Thanks for playing!")
+print("You got the correct lyrics in", counter, "attempt(s).")
